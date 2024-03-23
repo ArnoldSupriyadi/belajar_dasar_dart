8e783dbd7e3510e5dade2750d6bc7fb64e051c9b
Arnold Supriyadi <arnoldsupriyadi@Arnold-Macbook.local>
2024-03-23 15:57:14 +0700

    baru sampai video operator aritmatika

---
+++ bin/string.dart
@@ -0,0 +1,27 @@
+void main() {
+  String firstName = 'Eko';
+  String lastName = "Arnold";
+
+  print(firstName);
+  print(lastName);
+
+  var fullName = '$firstName Jamet $lastName';
+
+  print(fullName);
+
+  var text = 'this is \'dart\' \$ cool';
+  print(text);
+
+  var name1 = firstName + lastName;
+  var name2 = 'Arnold' 'Supriyadi';
+
+  print(name1);
+  print(name2);
+
+  var longString = '''
+  string ini sangat panjang
+  sehingga sulit di buat dalam satu baris kode program
+  ''';
+
+  print(longString);
+}
