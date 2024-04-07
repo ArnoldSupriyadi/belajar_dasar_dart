1e06c4cf55d260245bd299dcd98833cb322ec4fa
ArnoldSupriyadi <arnoldsupriyadi@gmail.com>
2024-04-07 12:43:01 +0700

    baru sampe video 33. For in

---
+++ bin/for_loop.dart
@@ -0,0 +1,13 @@
+void main() {
+  // for (;;) {
+  //   print('Perulangan Tanpa Henti');
+  // }
+
+  var counter = 1;
+
+  for (; counter <= 10;) {
+    print('Perulangan ke-$counter');
+
+    counter++;
+  }
+}
