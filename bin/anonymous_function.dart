39dd47469fd1eee273ea724b19b962a4bf3f6605
ArnoldSupriyadi <arnoldsupriyadi@gmail.com>
2024-04-20 09:20:35 +0700

    finish dart basic

--- bin/anonymous_function.dart
+++ bin/anonymous_function.dart
@@ -1,10 +1,23 @@
+void sayHello(String name, String Function(String) filter) {
+  var filteredName = filter(name);
+  print('Hi $filteredName');
+}
+
 void main() {
+  sayHello('Arnold Supriyadi', (name) {
+    return name.toUpperCase();
+  });
+
+  sayHello('Arnold Tampan', (name) => name.toLowerCase());
+
   var upperFunction = (String name) {
     return name.toUpperCase();
   };
 
   var lowerFunction = (String name) => name.toLowerCase();
 
-  print(upperFunction('Eko'));
-  print(lowerFunction('EKo'));
+  var result1 = (upperFunction('Eko'));
+  print(result1);
+  var result2 = (lowerFunction('Eko'));
+  print(result2);
 }
